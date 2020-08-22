75a132f9d944876deddc854414c0dbb7490e44cf
Yonathen <yonstudy18@gmail.com>
2020-08-22 05:16:10 -0700

    Change file permission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { R } from 'api/server/lib/response-service';
-import { UserAccount } from 'api/server/models/user-account';
-import { UserType, AccountStatus } from 'api/server/models/user';
 
 @Component({
   selector: 'app-root',
@@ -12,26 +9,6 @@ export class AppComponent implements OnInit {
   title = 'loide';
 
   ngOnInit() {
-    let data: UserAccount = {
-      email: 'tst3@gmail.com',
-      password: 'tst135',
-      profile: {
-        firstName: 'Jhon',
-        lastName: 'Doe',
-        type: UserType.Member,
-        notifications: [],
-        status: AccountStatus.Active
-      }
-    }
-
-    Meteor.call('createAccount', data, (error, result: R) => {
-      if (result && result.success) {
-        console.log(result);
-      } else if (result && !result.success) {
-        console.warn(result);
-      } else if (error) {
-        console.error(error);
-      }
-    });
+    
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,23 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @NgModule({
   declarations: [HeaderComponent, SidebarComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TranslateModule
+  ], 
+  exports: [
+    HeaderComponent,
+    SidebarComponent, 
+    FooterComponent,
+
   ]
 })
 export class SharedModule { }
