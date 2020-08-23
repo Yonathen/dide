017c65ac3f734110749a9ed692a952b648c1e3b6
Yonathen <yonstudy18@gmail.com>
2020-08-22 23:47:24 -0700

    Refactor code and set up router link

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,10 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from '../shared/shared.module';
-
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, SidebarComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
@@ -31,7 +30,7 @@ import {SidebarModule} from 'primeng/sidebar';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SidebarComponent, FooterComponent, PageNotFoundComponent],
+  declarations: [HeaderComponent, FooterComponent, PageNotFoundComponent],
   imports: [
     BrowserAnimationsModule,
     CommonModule,
@@ -63,7 +62,6 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
   ], 
   exports: [
     HeaderComponent,
-    SidebarComponent, 
     FooterComponent,
     BrowserAnimationsModule,
     FormsModule,
