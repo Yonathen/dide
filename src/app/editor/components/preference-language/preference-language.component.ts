8de79585a679c68623bf22407426ebfc5065f622
Yonathen Mekbib <yonstudy18@gmail.com>
2020-10-19 20:31:39 +0200

    Implimentation of preference in editor

--- src/app/editor/components/preference-language/preference-language.component.ts
+++ src/app/editor/components/preference-language/preference-language.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { SelectItem } from 'primeng/api/selectitem';
 import { PreferencesService } from 'src/app/preferences/services/preferences.service';
@@ -6,6 +7,8 @@ import { SettingSolver } from 'api/server/models/setting-solver';
 import { SettingExecutor } from 'api/server/models/setting-executor';
 import { SettingPreference } from 'api/server/models/setting-preference';
 import { AccountService } from 'src/app/shared/services/account.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-preference-language',
@@ -14,7 +17,7 @@ import { AccountService } from 'src/app/shared/services/account.service';
 })
 export class PreferenceLanguageComponent implements OnInit {
 
-
+  public visible: boolean;
   public languageOpt: SelectItem[] = [];
   public solverOpt: SelectItem[] = [];
   public executorOpt: SelectItem[] = [];
@@ -22,8 +25,12 @@ export class PreferenceLanguageComponent implements OnInit {
   public optionValues: string = '';
   public optionsTableColumns: any [];
   public preference: SettingPreference;
+  public savedPreference: SettingPreference;
+
+  public updatePreferenceForm: FormGroup;
 
   constructor(
+    private formBuilder: FormBuilder,
     private changeDetectorRef: ChangeDetectorRef,
     private preferenceService: PreferencesService,
     private accountService: AccountService) { }
@@ -31,8 +38,6 @@ export class PreferenceLanguageComponent implements OnInit {
   ngOnInit(): void {
 
     this.loadLanguages();
-    this.loadSolvers();
-    this.loadExecutors();
 
     this.optionsTableColumns = [
       { field: 'options', header: 'preference.options' }
@@ -40,7 +45,17 @@ export class PreferenceLanguageComponent implements OnInit {
 
     this.accountService.preference.subscribe(value => {
       this.preference = value;
+      this.savedPreference = _.clone(value);
     });
+  }
+
+  setUpForm() {
+    this.updatePreferenceForm = this.formBuilder.group({
+      executor: [ {value: this.preference.executor, disabled: false}, Validators.required ],
+      language: [ {value: this.preference.programingLanguage, disabled: false}, Validators.required ],
+      solver: [ {value: this.preference.solver, disabled: false}, Validators.required ]
+    });
+    this.visible = true;
   }
 
   loadLanguages() {
@@ -49,11 +64,12 @@ export class PreferenceLanguageComponent implements OnInit {
         const languages: SettingLanguage[] = R.returnValue;
         languages.forEach(language => {
           const item: SelectItem = { label: language.label, value: language };
-          if (language.type === LanguageType.Human) {
+          if (language.type === LanguageType.Programing) {
             this.languageOpt.push(item);
           }
         });
       }
+      this.loadSolvers();
     });
   }
 
@@ -66,6 +82,7 @@ export class PreferenceLanguageComponent implements OnInit {
           this.solverOpt.push(item);
         });
       }
+      this.loadExecutors();
     });
   }
 
@@ -78,13 +95,60 @@ export class PreferenceLanguageComponent implements OnInit {
           this.executorOpt.push(item);
         });
       }
+      this.setUpForm();
     });
 
+  }
+
+  onChangeOption(event, prop: string) {
+    switch ( prop ) {
+      case 'language':
+        this.preference.programingLanguage = event.value;
+        break;
+      case 'solver':
+        this.preference.solver = event.value;
+        break;
+      case 'executor':
+        this.preference.executor = event.value;
+    }
   }
 
   addOptionValues() {
     this.preference.other.push({ options: this.optionValues });
     this.optionValues = '';
+  }
+
+  isVisible(): boolean {
+    return this.visible;
+  }
+
+  isChanged(): boolean {
+    if ( this.preference.language._id !== this.savedPreference.language._id ) {
+      return true;
+    } else if ( this.preference.solver._id !== this.savedPreference.solver._id ) {
+      return true;
+    } else if ( this.preference.executor._id !== this.savedPreference.executor._id ) {
+      return true;
+    } else if ( this.preference.other.length !== this.savedPreference.other.length ) {
+      return true;
+    } else {
+      for ( let opt of this.preference.other ) {
+        const indexOpt = this.savedPreference.other.findIndex( value => value.options === opt.options );
+        if ( indexOpt < 0 ) {
+          return true;
+        }
+      }
+    }
+  }
+
+  reset() {
+    this.visible = false;
+    this.accountService.getUserPreferences().then(R => {
+      if ( R.success ) {
+        this.setUpForm();
+      }
+      this.visible = true;
+    });
   }
 
 }
